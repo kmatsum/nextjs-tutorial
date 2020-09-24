3d89f6870b995a3db41c2a3a2ea5323e544509d7
Kazushi <kazushi.matsumoto@gmail.com>
2020-09-24 18:46:04 -0500

    Initial Setup of a very basic NextJS application

---
+++ pages/auth/index.js
@@ -0,0 +1,15 @@
+// Standard Imports
+import React from 'react';
+
+
+
+// Component Function =========================
+const authIndexPage = (props) => {
+    
+    // Return JSX ----------
+    return (
+        <div>
+            <h1>The Auth Index Page</h1>
+        </div>
+    );
+}; export default authIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// Standard Imports
+import React from 'react';
+
+
+
+// Component Function =========================
+const indexPage = (props) => {
+    
+    // Return JSX ----------
+    return (
+        <div>
+            <h1>The Main Page</h1>
+        </div>
+    );
+}; export default indexPage;
