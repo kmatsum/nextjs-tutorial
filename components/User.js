b2232f968b9df806543b14c57dcc082bce906a43
Kazushi <kazushi.matsumoto@gmail.com>
2020-09-26 18:36:42 -0500

    Added some in-line CSS

--- components/User.js
+++ components/User.js
@@ -9,6 +9,16 @@ const user = (props) => {
         <div>
             <h1>{props.name}</h1>
             <p>Age: {props.age}</p>
+
+            {/* In-Line JSX CSS Styling */}
+            <style jsx>{`
+                div {
+                    border: 1px solid #eee;
+                    box-shadow: 0 2p 3px #ccc;
+                    padding: 20px;
+                    text-align: center;
+                }
+            `}</style>
         </div>
     );
 }; export default user;
