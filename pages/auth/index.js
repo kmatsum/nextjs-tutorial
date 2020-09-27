c2e222219b1a26735b4f6f44e8c6de60a72be769
Kazushi <kazushi.matsumoto@gmail.com>
2020-09-26 20:47:49 -0500

    Added a getInitialProps Lifecycle Hook

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -7,7 +7,18 @@ const authIndexPage = (props) => {
     // Return JSX ----------
     return (
         <div>
-            <h1>The Auth Index Page</h1>
+            <h1>The Auth Index Page - {props.appName}</h1>
         </div>
     );
-}; export default authIndexPage;
+};
+
+authIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: "Super App (Auth)" });
+        }, 1000);
+    });
+    return promise;
+};
+
+export default authIndexPage;
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,21 @@ import Router from 'next/router';
 
 // Component Function =========================
 class IndexPage extends React.Component {
+
+    static async getInitialProps(context) {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({ appName: "Super App" });
+            }, 1000);
+        });
+        return promise;
+    }
+
     render() {
         // Return JSX ----------
         return (
             <div>
-                <h1>The Main Page</h1>
+                <h1>The Main Page of {this.props.appName}</h1>
                 <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
                 <button onClick={() => {
                     Router.push('/auth');
